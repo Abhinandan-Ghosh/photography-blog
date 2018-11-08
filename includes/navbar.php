1eb93b0de44c79e75bf8c70b097272b03980f4bf
calclint <calclint@users.noreply.github.com>
2018-11-08 16:55:31 +0530

    Basic Modals Ready
    At the end of style.css I have added pad-left class to give padding to both Modal. ( text is centered but due to the "Close button" h4 tag is not centered)
    
    But it is not working so I added style atrribute directly to both the titles.

--- includes/navbar.php
+++ includes/navbar.php
@@ -24,25 +24,25 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header text-center">
-        <h4 class="modal-title w-100 font-weight-bold">Write to us</h4>
+        <h4 class="modal-title w-100 font-weight-bold pad-left " style="padding-left: 32px;">Write to us</h4>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body mx-3">
-        <div class="md-form mb-5">
+        <div class="md-form ">
           <i class="fa fa-user prefix grey-text"></i>
           <input type="text" id="form34" class="form-control validate">
           <label data-error="wrong" data-success="right" for="form34">Your name</label>
         </div>
 
-        <div class="md-form mb-5">
+        <div class="md-form ">
           <i class="fa fa-envelope prefix grey-text"></i>
           <input type="email" id="form29" class="form-control validate">
           <label data-error="wrong" data-success="right" for="form29">Your email</label>
         </div>
 
-        <div class="md-form mb-5">
+        <div class="md-form ">
           <i class="fa fa-tag prefix grey-text"></i>
           <input type="text" id="form32" class="form-control validate">
           <label data-error="wrong" data-success="right" for="form32">Subject</label>
@@ -56,7 +56,7 @@
 
       </div>
       <div class="modal-footer d-flex justify-content-center">
-        <button class="btn btn-unique">Send <i class="fa fa-paper-plane-o ml-1"></i></button>
+        <button class="btn btn-dark">Send <i class="fa fa-paper-plane-o ml-1"></i></button>
       </div>
     </div>
   </div>
