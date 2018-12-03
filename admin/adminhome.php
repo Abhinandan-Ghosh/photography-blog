04b026ad7e5d0fed9caac79c47444d48d4f14bd9
Abhinandan-Ghosh <35860824+Abhinandan-Ghosh@users.noreply.github.com>
2018-12-03 09:51:04 +0530

    Tiny changes in Admin home

--- admin/adminhome.php
+++ admin/adminhome.php
@@ -85,7 +85,8 @@ else{
                 </div>
             </nav>
 
-           
+           <h2 class="text-center">Messages</h2>
+           <hr>
             <div class = "table-responsive">
             <table class="table table-striped table-dark">
                 <thead class="thead-dark">
@@ -119,7 +120,7 @@ $count++;
         </div>
     </div>
 
-<?php echo mysqli_num_rows($result) ?>
+
 
     <!-- jQuery CDN - Slim version (=without AJAX) -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
